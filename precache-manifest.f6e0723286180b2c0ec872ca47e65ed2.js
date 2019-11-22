9034a0ae390ded72275cdf65e9aea3392b60129b
V MukeshSanghani <v.mukeshsanghani@perficient.com>
2019-11-22 11:36:38 +0530

    Updates

---
+++ precache-manifest.f6e0723286180b2c0ec872ca47e65ed2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e966545bcfa821ca72f9d17f5e022ebb",
+    "url": "/vishesh_sanghani.com/index.html"
+  },
+  {
+    "revision": "1be92484af3cb6cede9e",
+    "url": "/vishesh_sanghani.com/static/css/main.6ce78ccb.chunk.css"
+  },
+  {
+    "revision": "55d3dd3d5489da9f9249",
+    "url": "/vishesh_sanghani.com/static/js/2.fda26e51.chunk.js"
+  },
+  {
+    "revision": "1be92484af3cb6cede9e",
+    "url": "/vishesh_sanghani.com/static/js/main.b3877ccf.chunk.js"
+  },
+  {
+    "revision": "6a6e7a8c3e20ecd20e87",
+    "url": "/vishesh_sanghani.com/static/js/runtime-main.14cb20cb.js"
+  }
+]);
